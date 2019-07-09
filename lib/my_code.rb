7802af4aba8ef8662b889ef534d2731d95c4ffff
Curtis Arnold <arnold.curtis@gmail.com>
2019-07-09 19:03:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,7 +10,8 @@ def map(source_array)
 end 
 
 def reduce(source_array, *starting_point)
-if source_array.include?(true) || source_array.include?(false) || source_array[0] = false 
+if (source_array.include?(true) || source_array.include?(false) ||
+source_array[0] == false) 
     boolean = !!source_array[0]  
     i = 0 
     while (i < source_array.length) do 
